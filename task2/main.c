1699bca7ff60d4b7850a287d1c6271d77103792c
Danya <danyakulazhenko835@gmail.com>
2023-09-25 23:22:07 +0300

    some bug fixes

--- task2/main.c
+++ task2/main.c
@@ -5,15 +5,12 @@
 #define NUMS_COUNT 3
 
 int playOneRound(int bet);
-int generateRandomSymbol();
 
 int main() {
-    srand(time(NULL));
-
     int balance = 1000;
 
     while (balance > 0) {
-        printf("Your balance: %d\n", balance);
+        printf("Your balance: %d$\n", balance);
 
         int bet;
         printf("Make a bet (or 0 to exit):");
@@ -27,19 +24,23 @@ int main() {
             printf("Your bet is bigger than balance, try again.\n");
             continue;
         }
+        balance -= bet;
 
         int result = playOneRound(bet);
 
         if (result > 0) {
             balance += result;
-            printf("You won %d!\n", result);
+            if (result == bet) {
+                printf("You won your bet back(%d$)\n", result);
+            } else {
+                printf("You won %d$!\n", result);
+            }
         } else {
-            balance -= bet;
-            printf("You lost your bet.\n");
+            printf("You lost your bet (%d$)\n", bet);
         }
     }
 
-    printf("Game over!\n");
+    printf("Game over! Your final balance: %d$\n", balance);
 
     return 0;
 }
@@ -49,7 +50,20 @@ int generateRandomSymbol() {
     return rand() % 10;
 }
 
+int isNumbersEqual(int *arr[]) {
+    for (int i = 0; i < NUMS_COUNT - 1; i++) {
+        if (arr[i] != arr[i + 1]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// returns 0 if user lost,
+// in other case return bet which user won
 int playOneRound(int bet) {
+    srand(time(NULL));
+
     int symbols[NUMS_COUNT];
 
     printf("Random numbers: ");
@@ -62,25 +76,27 @@ int playOneRound(int bet) {
     printf("\n");
 
 
-    if (symbols[0] == symbols[1] && symbols[1] == symbols[2]) {
+
+    if (isNumbersEqual((int **) &symbols) == 1) {
         if (symbols[0] == 7) {
+            printf("Jackpot!\n");
             return bet * 3;
         }
         return bet * 2;
     }
 
     for (int i = 0; i < NUMS_COUNT; i++) {
-        if (symbols[i] == 7) {
-            return bet;
+        for (int j = i + 1; j < NUMS_COUNT; j++) {
+            if (symbols[i] == symbols[j]) {
+                return bet * 2;
+            }
         }
     }
 
 
     for (int i = 0; i < NUMS_COUNT; i++) {
-        for (int j = i + 1; j < NUMS_COUNT; j++) {
-            if (symbols[i] == symbols[j]) {
-                return bet;
-            }
+        if (symbols[i] == 7) {
+            return bet;
         }
     }
 
